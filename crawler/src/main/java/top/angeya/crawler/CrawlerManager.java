3813c0be165e2adc4b9787f1d09a9aa8727e5c63
5790 <wangaj@sciyon.com>
2023-10-20 15:14:39 +0800

    代码调整

--- crawler/src/main/java/top/angeya/crawler/CrawlerManager.java
+++ crawler/src/main/java/top/angeya/crawler/CrawlerManager.java
@@ -4,7 +4,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import top.angeya.config.CrawlerConfig;
-import top.angeya.service.CommonWebDataService;
 import us.codecraft.webmagic.Spider;
 
 import javax.annotation.PostConstruct;

f631a27af65f7c5191b5a9a75acd7ba20e15d974
angeya <1571858518@qq.com>
2023-11-08 23:51:50 +0800

    增加未处理的url的相关基础代码

---
+++ crawler/src/main/java/top/angeya/dao/UnprocessedUrlMapper.java
@@ -0,0 +1,14 @@
+package top.angeya.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+import top.angeya.entity.UnprocessedUrl;
+
+/**
+ * @author: angeya
+ * @date: 2023/11/8 23:47
+ * @description:
+ */
+@Repository
+public interface UnprocessedUrlMapper extends BaseMapper<UnprocessedUrl> {
+}
---
+++ crawler/src/main/java/top/angeya/service/UnprocessedUrlService.java
@@ -0,0 +1,15 @@
+package top.angeya.service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import top.angeya.dao.UnprocessedUrlMapper;
+import top.angeya.entity.UnprocessedUrl;
+
+/**
+ * @author: angeya
+ * @date: 2023/11/8 23:49
+ * @description:
+ */
+@Service
+public class UnprocessedUrlService extends ServiceImpl<UnprocessedUrlMapper, UnprocessedUrl> {
+}
